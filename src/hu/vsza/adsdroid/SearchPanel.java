5ce7f7de5fa2240d039b72566433ff720148d611
András Veres-Szentkirályi <vsza@vsza.hu>
2012-04-15 21:49:39 +0200

    implemented basic search PoC

--- src/hu/vsza/adsdroid/SearchPanel.java
+++ src/hu/vsza/adsdroid/SearchPanel.java
@@ -1,7 +1,16 @@
 package hu.vsza.adsdroid;
 
+import java.util.List;
+import java.io.IOException;
+import hu.vsza.adsapi.Search;
+import hu.vsza.adsapi.Part;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SearchPanel extends Activity
 {
@@ -11,5 +20,25 @@ public class SearchPanel extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		Spinner searchModeSpinner = (Spinner)findViewById(R.id.search_mode);
+		ArrayAdapter<Search.Mode> searchModeAdapter = new ArrayAdapter<Search.Mode>(this,
+				android.R.layout.simple_spinner_item, Search.Mode.values());
+		searchModeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		searchModeSpinner.setAdapter(searchModeAdapter);
+	}
+
+	public void searchByPartName(View view) {
+		Spinner searchModeSpinner = (Spinner)findViewById(R.id.search_mode);
+		Search.Mode selectedSearchMode = (Search.Mode)searchModeSpinner.getSelectedItem();
+		EditText partNameEditor = (EditText)findViewById(R.id.part_name);
+		String partName = partNameEditor.getText().toString();
+		try {
+			List<Part> results = Search.searchByPartName(partName, selectedSearchMode);
+			Toast.makeText(getBaseContext(), Integer.toString(results.size()) + " result(s) found",
+					Toast.LENGTH_SHORT).show();
+		} catch (IOException ioe) {
+			Toast.makeText(getBaseContext(), "Error fetching results: " + ioe.getMessage(),
+					Toast.LENGTH_SHORT).show();
+		}
 	}
 }
