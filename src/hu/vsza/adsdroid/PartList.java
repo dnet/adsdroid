3c678b0d5b76d1f5d8277958f6e4c1b28cffdbcb
András Veres-Szentkirályi <vsza@vsza.hu>
2012-04-17 15:31:57 +0200

    adsdroid.PartList: download PDF upon click

--- src/hu/vsza/adsdroid/PartList.java
+++ src/hu/vsza/adsdroid/PartList.java
@@ -1,17 +1,25 @@
 package hu.vsza.adsdroid;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.io.*;
+import java.net.URLConnection;
 import hu.vsza.adsapi.Part;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.AsyncTask;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 public class PartList extends ListActivity
 {
 	public final static String PARTS = "hu.vsza.adsdroid.PartList.PARTS";
+	ProgressDialog mProgressDialog;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -27,5 +35,67 @@ public class PartList extends ListActivity
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		Part selectedPart = new Part((Map<String, String>)getListView().getItemAtPosition(position));
+		mProgressDialog = new ProgressDialog(this);
+		mProgressDialog.setMessage("Starting download...");
+		mProgressDialog.setIndeterminate(true);
+		DownloadDatasheet dd = new DownloadDatasheet();
+		dd.execute(selectedPart);
+	}
+
+	private class DownloadDatasheet extends AsyncTask<Part, String, String> {
+
+		@Override
+		protected String doInBackground(Part... parts) {
+			Part selectedPart = parts[0];
+			String fileName = fileNameForPart(selectedPart);
+			try {
+				publishProgress("Fetching PDF URL...");
+				URLConnection pdfConnection = selectedPart.getPdfConnection();
+				publishProgress("Connecting to PDF server...");
+				pdfConnection.connect();
+				InputStream input = new BufferedInputStream(pdfConnection.getInputStream());
+				OutputStream output = new FileOutputStream(fileName);
+
+				byte data[] = new byte[1024];
+				long total = 0;
+				int count;
+				while ((count = input.read(data)) != -1) {
+					total += count;
+					publishProgress("Downloading datasheet... (fetched " + total + " bytes so far)");
+					output.write(data, 0, count);
+				}
+
+				output.flush();
+				output.close();
+				input.close();
+				return fileName;
+			} catch (Exception e) {
+				return null;
+			}
+		}
+
+		protected String fileNameForPart(Part part) {
+			return "/sdcard/" + part.get(Part.NAME) + DateFormat.format("-yyyyMMdd-kkmmss", new Date()) + ".pdf";
+		}
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			mProgressDialog.show();
+		}
+
+		@Override
+		protected void onProgressUpdate(String... progress) {
+			super.onProgressUpdate(progress);
+			mProgressDialog.setMessage(progress[0]);
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			mProgressDialog.dismiss();
+			Toast.makeText(getBaseContext(), result == null ? "Error happened during download" : "Datasheet saved as " + result,
+					Toast.LENGTH_SHORT).show();
+		}
 	}
 }
